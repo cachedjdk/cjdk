66c5b127cc9c42a14e953ca43b90d184305e7d08
Mark A. Tsuchida <matsuchida@wisc.edu>
2022-11-14 16:38:44 -0600

    Add Python 3.11 testing

--- noxfile.py
+++ noxfile.py
@@ -7,7 +7,7 @@ import nox
 nox.options.sessions = ["test"]
 
 
-@nox.session(python=["3.8", "3.9", "3.10"])
+@nox.session(python=["3.8", "3.9", "3.10", "3.11"])
 def test(session):
     session.install(".[testing]")
     session.run("pytest")
