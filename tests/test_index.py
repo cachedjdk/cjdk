b67016567fa00bfd76aaefbe229b6a8ca8c7f138
Curtis Rueden <ctrueden@wisc.edu>
2024-08-07 19:49:11 -0500

    Add tests for new functions and parameters
    _api:
    - _get_vendors
    - _get_jdks
    
    _conf:
    - configure: fallback_to_default_vendor=False
    
    _index:
    - jdk_url: exact_version=non-None
    - _postprocess_index
    - _match_versions

--- tests/test_index.py
+++ tests/test_index.py
@@ -87,6 +87,25 @@ def test_jdk_url():
         )
         == "tgz+https://example.com/a/b/c.tgz"
     )
+    assert (
+        _index.jdk_url(
+            index,
+            configure(
+                os="linux", arch="amd64", vendor="adoptium", version="17"
+            ),
+        )
+        == "tgz+https://example.com/a/b/c.tgz"
+    )
+    assert (
+        _index.jdk_url(
+            index,
+            configure(
+                os="linux", arch="amd64", vendor="adoptium", version="11"
+            ),
+            exact_version="17.0.1",
+        )
+        == "tgz+https://example.com/a/b/c.tgz"
+    )
 
 
 def test_cached_index_path(tmp_path):
@@ -138,6 +157,62 @@ def test_read_index(tmp_path):
     with open(path, "w") as outfile:
         json.dump(data, outfile)
     assert _index._read_index(path) == data
+
+
+def test_postprocess_index():
+    index = {
+        "linux": {
+            "amd64": {
+                "jdk@ibm-semeru-openj9-java11": {
+                    "11.0.21+9_openj9-0.41.0": "a",
+                    "11.0.22+7_openj9-0.43.0": "b",
+                    "11.0.23+9_openj9-0.44.0": "c",
+                },
+                "jdk@ibm-semeru-openj9-java17": {
+                    "17.0.1+12_openj9-0.29.1": "d",
+                    "17.0.2+8_openj9-0.30.0": "e",
+                    "17.0.3+7_openj9-0.32.0": "f",
+                },
+                "jdk@ibm-semeru-openj9-java21": {
+                    "21.0.1+12_openj9-0.42.0": "g",
+                    "21.0.2+13_openj9-0.43.0": "h",
+                },
+                "jdk@not-semeru": {"8.0.252": "i"},
+            }
+        }
+    }
+    pp_index = _index._postprocess_index(index)
+    assert pp_index is index
+    assert "jdk@ibm-semeru-openj9" in index["linux"]["amd64"]
+    assert len(index["linux"]["amd64"]["jdk@ibm-semeru-openj9"]) == 8
+
+
+def test_match_versions():
+    f = _index._match_versions
+    assert f("adoptium", ["10", "11.0", "11.1", "1.12.0"], "11") == {
+        (11, 0): "11.0",
+        (11, 1): "11.1",
+    }
+    assert f("adoptium", ["10", "11.0", "11.1", "1.12.0"], "12") == {
+        (12, 0): "1.12.0"
+    }
+    assert f("graalvm", ["10", "11.0", "11.1", "1.12.0"], "11") == {
+        (11, 0): "11.0",
+        (11, 1): "11.1",
+    }
+    assert f("graalvm", ["10", "11.0", "11.1", "1.12.0"], "1") == {
+        (1, 12, 0): "1.12.0"
+    }
+    assert f("temurin", ["11.0", "17.0", "18.0"], "") == {
+        (11, 0): "11.0",
+        (17, 0): "17.0",
+        (18, 0): "18.0",
+    }
+    assert f("temurin", ["11.0", "17.0", "18.0"], "17+") == {
+        (17, 0): "17.0",
+        (18, 0): "18.0",
+    }
+    assert f("temurin", ["11.0", "17.0", "18.0"], "19+") == {}
 
 
 def test_match_version():
