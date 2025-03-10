4601162985b69d8669c5043b22eeea2c25ef0167
Mark A. Tsuchida <matsuchida@wisc.edu>
2025-03-10 13:57:40 -0500

    Add Python 3.13 testing

--- noxfile.py
+++ noxfile.py
@@ -6,8 +6,10 @@ import nox
 
 nox.options.sessions = ["test"]
 
+nox.options.default_venv_backend = "uv|virtualenv"
 
-@nox.session(python=["3.9", "3.10", "3.11", "3.12"])
+
+@nox.session(python=["3.9", "3.10", "3.11", "3.12", "3.13"])
 def test(session):
     session.install(".[testing]")
     session.run("pytest")
